45af9b5167659b8c3c112aea7ab974200208eabf
Brennen Green <brennengreen@outlook.com>
2022-03-26 18:02:15 -0400

    Connecting to server with GUI

--- client.py
+++ client.py
@@ -3,13 +3,37 @@ import sys
 from PyQt5.QtWidgets import (QMainWindow, QLineEdit, QGridLayout, QWidget, QToolTip, QPushButton, QApplication)
 from PyQt5.QtGui import QFont
 
+
+#  Test Stuff
+#HOST = "127.0.0.1"
+#PORT = 65432
+#
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
     
     def connect(self):
-        print("CONNECT")
+        if self.host_edit.text() == '':
+            self.host = "127.0.0.1"
+        else:
+            self.host = self.host_edit.text()
+        
+        if self.port_edit.text() == '':
+            self.port = 65432
+        else:
+            self.port = int(self.port_edit.text())
+
+        print(f'Connecting too... {self.host}:{self.port}')
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
+            try:
+                self.s.connect((self.host, self.port))
+                self.s.sendall(b"Hello, world")
+                data = self.s.recv(1024)
+                print(f'RECV\'D FROM SERVER: {data}')
+            except ConnectionRefusedError:
+                print(f'Connection to {self.host}:{self.port} refused by machine')
 
     def upload(self):
         print("UPLOAD")
@@ -26,47 +50,47 @@ class Window(QMainWindow):
     def initUI(self):
         QToolTip.setFont(QFont('SansSerif', 10))
 
-        cnct_btn = QPushButton('CONNECT', self)
-        cnct_btn.setToolTip('Connect to IP Address on specified PORT')
-        cnct_btn.resize(cnct_btn.sizeHint())
-        cnct_btn.clicked.connect(self.connect)
+        self.cnct_btn = QPushButton('CONNECT', self)
+        self.cnct_btn.setToolTip('Connect to IP Address on specified PORT')
+        self.cnct_btn.resize(self.cnct_btn.sizeHint())
+        self.cnct_btn.clicked.connect(self.connect)
         
-        host_edit = QLineEdit()
-        port_edit = QLineEdit()
+        self.host_edit = QLineEdit()
+        self.port_edit = QLineEdit()
 
-        upld_btn = QPushButton('UPLOAD', self)
-        upld_btn.setToolTip('Upload specified file from local folder')
-        upld_btn.resize(cnct_btn.sizeHint())
-        upld_btn.clicked.connect(self.upload)
+        self.upld_btn = QPushButton('UPLOAD', self)
+        self.upld_btn.setToolTip('Upload specified file from local folder')
+        self.upld_btn.resize(self.cnct_btn.sizeHint())
+        self.upld_btn.clicked.connect(self.upload)
 
-        dnld_btn = QPushButton('DOWNLOAD', self)
-        dnld_btn.setToolTip('Download specified file from shared folder')
-        dnld_btn.resize(dnld_btn.sizeHint())
-        dnld_btn.clicked.connect(self.download)
+        self.dnld_btn = QPushButton('DOWNLOAD', self)
+        self.dnld_btn.setToolTip('Download specified file from shared folder')
+        self.dnld_btn.resize(self.dnld_btn.sizeHint())
+        self.dnld_btn.clicked.connect(self.download)
 
-        delt_btn = QPushButton('DELETE', self)
-        delt_btn.setToolTip('Download specified file from shared folder')
-        delt_btn.resize(delt_btn.sizeHint())
-        delt_btn.clicked.connect(self.delete)
+        self.delt_btn = QPushButton('DELETE', self)
+        self.delt_btn.setToolTip('Download specified file from shared folder')
+        self.delt_btn.resize(self.delt_btn.sizeHint())
+        self.delt_btn.clicked.connect(self.delete)
 
-        dir_btn = QPushButton('DIR', self)
-        dir_btn.setToolTip('List directory contents')
-        dir_btn.resize(dir_btn.sizeHint())
-        dir_btn.clicked.connect(self.dir)
+        self.dir_btn = QPushButton('DIR', self)
+        self.dir_btn.setToolTip('List directory contents')
+        self.dir_btn.resize(self.dir_btn.sizeHint())
+        self.dir_btn.clicked.connect(self.dir)
 
-        grid = QGridLayout()
-        grid.addWidget(cnct_btn, 1, 0)
-        grid.addWidget(host_edit, 1, 1)
-        grid.addWidget(port_edit, 1, 3)
+        self.grid = QGridLayout()
+        self.grid.addWidget(self.cnct_btn, 1, 0)
+        self.grid.addWidget(self.host_edit, 1, 1)
+        self.grid.addWidget(self.port_edit, 1, 3)
 
-        grid.addWidget(upld_btn, 2, 0)
-        grid.addWidget(dnld_btn, 3, 0)
-        grid.addWidget(delt_btn, 4, 0)
-        grid.addWidget(dir_btn, 5, 0)
+        self.grid.addWidget(self.upld_btn, 2, 0)
+        self.grid.addWidget(self.dnld_btn, 3, 0)
+        self.grid.addWidget(self.delt_btn, 4, 0)
+        self.grid.addWidget(self.dir_btn, 5, 0)
 
-        central_wid = QWidget(self)
-        self.setCentralWidget(central_wid)
-        central_wid.setLayout(grid)
+        self.central_wid = QWidget(self)
+        self.setCentralWidget(self.central_wid)
+        self.central_wid.setLayout(self.grid)
         self.setGeometry(300, 300, 600, 600)
         self.setWindowTitle("Client Interface")
         self.show()
